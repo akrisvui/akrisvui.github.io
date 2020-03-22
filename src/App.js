b61cbf01f451dffdf18e9ff1b1e25599ada1e06d
akrisvui <34187758+akrisvui@users.noreply.github.com>
2020-03-22 13:19:20 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -36,11 +36,20 @@ const App = () => {
 	const perm = () => {
 
 		const connection = mysql.createConnection({
-  		host: "localhost",
-  		user: "root",
-  		database: "usersdb",
-  		password: "пароль_от_сервера"
+  		host: "redactorhelp.ru",
+  		user: "u0991630_sima",
+  		database: "u0991630_redactor",
+  		password: "nasralkorol"
 });
+
+		 connection.connect(function(err){
+		    if (err) {
+		      return console.error("Ошибка: " + err.message);
+		    }
+		    else{
+		      console.log("Подключение к серверу MySQL успешно установлено");
+		    }
+		 });
 
 
 		/*//bridge.send("VKWebAppShowWallPostBox", { "owner_id ": -183268158, "message": "Hello!"});
